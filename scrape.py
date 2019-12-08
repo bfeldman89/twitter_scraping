13c7c3bccea1406eb5f21e4f14f9f0de6f56b2eb
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-07 20:12:52 -0600

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,47 +1,52 @@
+# !/usr/bin/env python3
+"""This module does blah blah."""
+import datetime
+import json
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from time import sleep
-import json
-import datetime
-
 
 # edit these three variables
-user = 'realdonaldtrump'
-start = datetime.datetime(2010, 1, 1)  # year, month, day
-end = datetime.datetime(2016, 12, 7)  # year, month, day
+start = datetime.datetime(2018, 1, 1)  # year, month, day
+end = datetime.datetime(2019, 12, 7)  # year, month, day
 
 # only edit these if you're having problems
-delay = 1  # time to wait on each page load before reading the page
+delay = 20  # time to wait on each page load before reading the page
 driver = webdriver.Safari()  # options are Chrome() Firefox() Safari()
 
 
 # don't mess with this stuff
 twitter_ids_filename = 'all_ids.json'
 days = (end - start).days + 1
+months = int(days / 30)
 id_selector = '.time a.tweet-timestamp'
 tweet_selector = 'li.js-stream-item'
-user = user.lower()
 ids = []
+
 
 def format_day(date):
     day = '0' + str(date.day) if len(str(date.day)) == 1 else str(date.day)
-    month = '0' + str(date.month) if len(str(date.month)) == 1 else str(date.month)
+    month = '0' + str(date.month) if len(str(date.month)
+                                         ) == 1 else str(date.month)
     year = str(date.year)
     return '-'.join([year, month, day])
 
-def form_url(since, until):
-    p1 = 'https://twitter.com/search?f=tweets&vertical=default&q=from%3A'
-    p2 =  user + '%20since%3A' + since + '%20until%3A' + until + 'include%3Aretweets&src=typd'
-    return p1 + p2
+
+def form_url(user, since, until):
+    root = 'https://twitter.com'
+    q1 = f"from%3A{user}%20since%3A{since}%20until%3A{until}&src=typed_query&f=video"
+    return f"{root}/search?q={q1}"
+
 
 def increment_day(date, i):
     return date + datetime.timedelta(days=i)
 
-for day in range(days):
+
+for instance in range(months):
     d1 = format_day(increment_day(start, 0))
-    d2 = format_day(increment_day(start, 1))
-    url = form_url(d1, d2)
+    d2 = format_day(increment_day(start, 30))
+    url = form_url('bfeldman89', d1, d2)
     print(url)
     print(d1)
     driver.get(url)
@@ -53,7 +58,8 @@ for day in range(days):
 
         while len(found_tweets) >= increment:
             print('scrolling down to load more tweets')
-            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            driver.execute_script(
+                'window.scrollTo(0, document.body.scrollHeight);')
             sleep(delay)
             found_tweets = driver.find_elements_by_css_selector(tweet_selector)
             increment += 10
@@ -62,15 +68,16 @@ for day in range(days):
 
         for tweet in found_tweets:
             try:
-                id = tweet.find_element_by_css_selector(id_selector).get_attribute('href').split('/')[-1]
-                ids.append(id)
+                tid = tweet.find_element_by_css_selector(
+                    id_selector).get_attribute('href').split('/')[-1]
+                ids.append(tid)
             except StaleElementReferenceException as e:
                 print('lost element reference', tweet)
 
     except NoSuchElementException:
         print('no tweets on this day')
 
-    start = increment_day(start, 1)
+    start = increment_day(start, 30)
 
 
 try:
